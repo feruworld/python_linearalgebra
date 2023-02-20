51171a0ebbc93ecb0357d700ef70039ab074e86f
FeruWorld <feru54604@gmail.com>
2023-02-20 21:57:15 +0900

    first commit

---
+++ 1.1_NumPyMatrixBasics.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array([[1, 4, -3], [-2, 1, 2]])
+b = np.array([[2, -3, 5], [-1, 4, 1]])
+
+result = 2*a - 3*b
+print(result)
---
+++ 1.2_NumPyZeroOneMatrix.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+zerovector = np.zeros(8)
+zeromatrix = np.zeros([3,4])
+onematrix = np.ones([5,3])
+
+print("8-dimensional zero vector")
+print(zerovector)
+print("3x4 zero matrix")
+print(zeromatrix)
+print("A 5x3 matrix such that all elements are 1.")
+print(onematrix)
